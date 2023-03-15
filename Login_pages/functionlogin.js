6a1d14b7813803e69624cacf522b1b04fd8d0e17
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2023-03-15 12:17:15 -0500

    yes

--- Login_pages/functionlogin.js
+++ Login_pages/functionlogin.js
@@ -1,9 +1,12 @@
-function checkPassword()
+function checkValidation()
 {
-    if(document.getElementById('password').value == '123')
+    var isAdmin = document.getElementById("name").value;
+    var password = document.getElementById("password").value;
+    if ( isAdmin == "admin" && password == "123")
     {
-        alert('Redirecting to Main Page')
-            window.location.href = "https://www.youtube.com/watch?v=Jrg9KxGNeJY";
+        alert ("Redirecting to Admin Page");
+        window.location = "mainpage.html";
+        return false;
     }
     else
     {
