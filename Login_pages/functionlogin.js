f3b562760a76ffaa451377bd0b872b7934792219
AbsolutelyBeginner <huy.businesscontact@gmail.com>
2023-03-15 12:37:37 -0500

    changes to login pages, now will add main pages

--- Login_pages/functionlogin.js
+++ Login_pages/functionlogin.js
@@ -2,10 +2,21 @@ function checkValidation()
 {
     var isAdmin = document.getElementById("name").value;
     var password = document.getElementById("password").value;
+    var manager1 = document.getElementById("name").value;
+    var password1 = document.getElementById("password").value;
     if ( isAdmin == "admin" && password == "123")
     {
         alert ("Redirecting to Admin Page");
-        window.location = "mainpage.html";
+        //window.location = "C:/Users/huybu/OneDrive/Documents/Spring 2023/COSC 3380/DBMS Project/ThemePark_Team10/Login_pages";
+        window.location = "adminpage.html";
+        return false;
+    }
+    else
+    if( manager1 = "JoeBiden69" && password1 == "cutediepie1!")
+    {
+        alert ("Redirecting to Management Page");
+        //window.location = "C:/Users/huybu/OneDrive/Documents/Spring 2023/COSC 3380/DBMS Project/ThemePark_Team10/Login_pages";
+        window.location = "managerpage.html";
         return false;
     }
     else
@@ -13,4 +24,6 @@ function checkValidation()
         alert('Wrong Password!');
         return false;
     }
+    
+    
 }
